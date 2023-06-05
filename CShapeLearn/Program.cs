93f74ef91be3b0c022ef87d8c53ebace150fad23
Tien Sy <tiensy002@gmail.com>
2023-06-05 15:53:06 +0700

    day1

---
+++ CShapeLearn/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CShapeLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = new int[3];
+            for ( int i = 0; i < array.Length; i++ )
+            {
+                array[i] = i;
+            }
+            foreach(int i in array )
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(array[3]);
+
+        }
+       
+    }
+}
---
+++ CShapeLearn/generic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CShapeLearn
+{
+    class generic
+    {
+        static void Main(String[] args)
+        {
+            int a = 45;
+            int b = 9;
+            Console.WriteLine($"gia tri cua a la: {a}, gia tri cua b la: {b}");
+            swap( ref a, ref b );
+            Console.WriteLine($"gia tri cua a la: {a}, gia tri cua b la: {b}");
+
+        }
+        static void swap(ref int a,  ref int b)
+        {
+            int step = a;
+            a = b;
+            b = step;
+        }
+    }
+}
